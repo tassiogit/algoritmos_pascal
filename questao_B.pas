92601a42989799d07d053d502788ff004722a7e9
Zaraki <tassio.online@gmail.com>
2015-10-06 21:51:25 -0300

    add questão B

---
+++ questao_B.pas
@@ -0,0 +1,20 @@
+Program Pzim;
+var f, c:real;
+Begin
+  writeln('=======================================================');
+  writeln('==========  CONVERSOR DE TEMPERATURA (F2C)  ===========');
+  writeln();
+  
+	write('Temperatura em Fahrenheit: ');
+  read(f);
+  c := (f-32) * 5/9;
+  
+  writeln();
+  
+  
+  write('Este valor equivale a ', c:1:2, ' graus Celsius.');
+  
+  writeln();
+  writeln('=======================================================');
+  
+End.
